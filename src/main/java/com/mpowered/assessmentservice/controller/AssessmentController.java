8aa7cb9fb14af665b91558a2e71ff906e9b534d1
Shashikant <shashikant.m@mpoweredhealth.com>
2023-10-05 20:09:33 +0530

    cns-7853

--- src/main/java/com/mpowered/assessmentservice/controller/AssessmentController.java
+++ src/main/java/com/mpowered/assessmentservice/controller/AssessmentController.java
@@ -3,6 +3,8 @@ package com.mpowered.assessmentservice.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,8 @@ import com.mpowered.assessmentservice.constant.Constants;
 @RestController
 @RequestMapping(Constants.URL_V1 + Constants.ASSESSMENT_URL)
 public class AssessmentController {
-
+	Logger log = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping("/getAllAssessments")
 	public List getAssessmentsList() {
 		return new ArrayList();
