7108d95a0e49e1a7aca427cbe63697cbf3b58b78
Prabhu Darshan <prabhu.d@mpoweredhealth.com>
2023-10-07 13:19:56 +0530

    add version constant

--- src/main/java/com/mpowered/assessmentservice/constant/Constants.java
+++ src/main/java/com/mpowered/assessmentservice/constant/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
 		Pending, Draft, Completed, Expired,
 	}
 
-	public static final String URL_V1 = "v1";
+	public static final String URL_V1 = "/v1";
 	public static final String ASSESSMENT_URL = "/assessment";
 }
